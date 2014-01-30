dff4de176bf3a192f88e53ae7e689ffec884ed4f
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-01-31 01:38:34 +0200

    Added AJAX request check for routing rules

--- App/Router.hs
+++ App/Router.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module App.Router where
 
-import Control.Monad (msum)
+import Control.Monad (msum, guard)
 import Control.Monad.Trans (liftIO)
 
 import Happstack.Server
@@ -14,17 +16,26 @@ routes conn = msum [
         dir "comment"   $ rAddComment conn, 
         dir "posts"     $ rGetPosts conn,
         dir "post"      $ rgetPost conn,
-
-        serveDirectory DisableBrowsing ["index.html"] "static" 
+        serveDirectory DisableBrowsing ["index.html"] "static",
+        serveFile (asContentType "text/html") "static/index.html"
     ]
 
 -- Routing items
-rAddComment conn = method POST >> decodeBody postBodyPolicy >> parseComment conn
-rGetPosts   conn = fR $ getJSONResponse getPosts conn
-rgetPost    conn = path $ fR . getJSONResponse (getPost conn)
+rAddComment conn = requireAjax $ (rPost $ parseComment conn)
+rGetPosts   conn = requireAjax $ (fR $ getJSONResponse getPosts conn)
+rgetPost    conn = requireAjax $ (path $ fR . getJSONResponse (getPost conn))
 
 -- Helpers
+isAjax :: ServerPart ()
+isAjax = do
+    h <- getHeaderM "X-Requested-With"
+    guard (h == Just "XMLHttpRequest")
+
+requireAjax f = isAjax >> f
+
 fR f = fmap toResponse f
+
+rPost f = method POST >> decodeBody postBodyPolicy >> f
 
 postComment conn comment = liftIO $ addComment conn (comment :: Comment) 
 
--- index.hs
+++ index.hs
@@ -4,7 +4,7 @@ import Happstack.Server (simpleHTTP, nullConf, port)
 
 import App.Connect
 import App.Router
- 
+
 main = do
     conn <- dbConnect
     simpleHTTP nullConf {port = 80} $ routes conn
