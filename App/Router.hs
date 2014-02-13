cd53fade843d1c0570cc4f5be238699bbf7ba0c4
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-02-13 23:10:45 +0200

    [Server] Added user check rule; Modified Ajax check

--- App/Router.hs
+++ App/Router.hs
@@ -5,6 +5,9 @@ module App.Router where
 import Control.Monad (msum, guard)
 import Control.Monad.Trans (liftIO)
 
+import qualified Data.ByteString.Lazy.Char8 as L (pack)
+import Data.Digest.Pure.MD5
+
 import Happstack.Server
 
 import App.ORM
@@ -13,17 +16,21 @@ import App.Controller
 
 -- Routing rules
 routes conn = msum [
-        dir "comment"   $ rAddComment conn, 
-        dir "posts"     $ rGetPosts conn,
-        dir "post"      $ rgetPost conn,
+        isAjax >> msum [
+            dir "login"     $ rLogin conn, 
+            dir "comment"   $ rAddComment conn, 
+            dir "posts"     $ rGetPosts conn,
+            dir "post"      $ rgetPost conn
+        ],
         serveDirectory DisableBrowsing ["index.html"] "static",
         serveFile (asContentType "text/html") "static/index.html"
     ]
 
 -- Routing items
-rAddComment conn = requireAjax $ (rPost $ parseComment conn)
-rGetPosts   conn = requireAjax $ (fR $ getJSONResponse getPosts conn)
-rgetPost    conn = requireAjax $ (path $ fR . getJSONResponse (getPost conn))
+rAddComment conn = rPost $ parsePost postComment conn
+rLogin      conn = rPost $ parsePost loginUser conn
+rGetPosts   conn = fR $ getJSONResponse getPosts conn
+rgetPost    conn = path $ fR . getJSONResponse (getPost conn)
 
 -- Helpers
 isAjax :: ServerPart ()
@@ -31,12 +38,12 @@ isAjax = do
     h <- getHeaderM "X-Requested-With"
     guard (h == Just "XMLHttpRequest")
 
-requireAjax f = isAjax >> f
-
 fR f = fmap toResponse f
 
 rPost f = method POST >> decodeBody postBodyPolicy >> f
 
+parsePost f conn = fR . resultToJSON . withData $ f conn 
+
 postComment conn comment = liftIO $ addComment conn (comment :: Comment) 
 
-parseComment conn = fR . resultToJSON . withData $ postComment conn 
+loginUser conn user = liftIO $ checkUserLogin conn (user :: User)
