7a7e29c5580e7721834952ee8a2cd43783cd4ae4
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2014-02-13 23:08:14 +0200

    [Server] Added user check

--- App/ORM.hs
+++ App/ORM.hs
@@ -5,6 +5,8 @@ module App.ORM where
 import Data.Typeable (Typeable)
 import Data.Data (Data)
 import qualified Data.Text as T (unpack)
+import qualified Data.ByteString.Lazy.Char8 as L (pack)
+import Data.Digest.Pure.MD5
 
 import Happstack.Server
 import App.DbMySQL
@@ -64,10 +66,39 @@ instance FromData Comment where
             }
 
 addComment conn comment = do
-    res <- dbAdd conn (post comment, text comment, user_id comment)
+    res <- dbExec conn insertComment (post comment, text comment, user_id comment)
     return res
 
 getComments conn id = do
     comments <- dbGetP conn selectComments [id::Int]
     return [Comment post text user_id user_name date | (post,t,user_id,un, d) <- comments,
                                                        let [text, user_name, date] = map T.unpack [t,un,d]]
+
+data User = User {
+        u_id :: Int,
+        display_name :: String,
+        login :: String,
+        password :: String
+    }
+    deriving (Data, Typeable)
+
+nullUser :: User
+nullUser = User {
+        u_id = undefined,
+        display_name = "",
+        login = "",
+        password = ""
+    }
+
+instance FromData User where
+    fromData = do
+        l <- look "login"
+        p <- look "password"
+        return nullUser {
+                login = l,
+                password = show . md5 $ L.pack p
+            }
+
+checkUserLogin conn user = do
+    res <- dbGetP conn checkUser (login user, password user)
+    return $ head [User id name (login user) "" | (id,n) <- res, let name = T.unpack n]
